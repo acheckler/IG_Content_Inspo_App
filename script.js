57a9b5051b1d26a241406ca8d430f0007737ac1d
Allison Heckler <allison@Allisons-MBP.attlocal.net>
2020-12-09 09:03:40 -0600

    added color scheme selector

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
 'use strict'
-
 const unsplashID = "RB0c5XN-C4SiX0b5hQwEgq_-FSZ1zi00xCtLXON7UY0"
-const unsplashURL = "https://api.unsplash.com/photos/random"
+const unsplashURL = "https://api.unsplash.com/search/photos"
 const qgURL = "https://quote-garden.herokuapp.com/api/v2/"
-
-function generateWelcomePage() {
-
-  return `
-    <div class = "welcome-page">
-      <h2> Hello! <h2>
-        <p> This app is for those whom have an idea, or topic, they'd like to post about, but they need the content for it! <p>
-        <p> Simply fill out the search form using your topic as a keyword (e.g. reading, yoga, coffee, etc) and you'll recieve pictures and quotes related to your topic! <p>
-        <p> Take the content from this app, edit it to your asthetic desires, and now you've got an Instagram post or story! <p>
-        <button type = "button" id = "start"> Let's go! </button>
-    </div>
-  `
-}
-
-function watchStartButton() {
-  $('.welcome-page').on('click', '#start', function(event) {
-    event.preventDefault();
-    render();
-  });
-}
 function watchForm() {
-  
   $('#js-form').submit(event => {
     event.preventDefault();
     var picSearchTerm = $('#photo-search-term').val();
@@ -34,6 +12,8 @@ function watchForm() {
     console.log(quoteSearchType);
     getPictures(picSearchTerm, picColor);
     getQuotes(quoteSearchTerm, quoteSearchType);
+    $('.photos').empty();
+    $('.quotes').empty();
   });
 }
 
@@ -42,21 +22,18 @@ function formatQueryParams(params) {
         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
             return queryItems.join('&');
 }
-
 function getPictures(picSearchTerm, picColor) {
   const params = {
     client_id: unsplashID,
     query: picSearchTerm,
-    count: 3,
-  }
-  if (picColor != "none") {
-    params.color = picColor
+    per_page: 50,
+    color: picColor
   }
   const queryString = formatQueryParams(params);
   const url = unsplashURL + '?' + queryString;
-  console.log(url);
-
-  
+const getRandom = max => {
+  return Math.ceil(Math.random() * max)
+}
   fetch(url)
   .then(response => {
     if (response.ok) {
@@ -65,13 +42,16 @@ function getPictures(picSearchTerm, picColor) {
      throw new Error(response.statusText);
     })
     .then(responseJson => {
-      displayPictures(responseJson);
+      let total = responseJson.results.length
+      let random = getRandom(total)
+      // console.log(responseJson, 'blehhh')
+      //console.log(total, 'total')
+      displayPictures(responseJson.results[random]);
     })
     .catch(err => {
-        $('#error-message').text(`Could not find city/location. Please enter a different city/location.`);
+        $('#error-message').text(`Could not find a photo with given keyword(s). Please try using another search term.`);
     });  
 }
-
 function getQuotes(quoteSearchTerm, quoteSearchType) {
   const params = {
     page: 1,
@@ -79,8 +59,6 @@ function getQuotes(quoteSearchTerm, quoteSearchType) {
   }
     const queryString = formatQueryParams(params);
   const url = qgURL + quoteSearchType + '/' + quoteSearchTerm + '?' + queryString;
-  console.log(url);
-
 fetch(url)
   .then(response => {
     if (response.ok) {
@@ -92,40 +70,30 @@ fetch(url)
       displayQuotes(responseJson);
     })
     .catch(err => {
-        $('#error-message').text(`Could not find city/location. Please enter a different city/location.`);    
+        $('#error-message').text(`Could not find a quote with given keyword(s). Please try using another search term.`);    
 });
 }
-
 function displayQuotes(responseJson) {
-  $('.quote-list').empty();
+  console.log(responseJson, 'hello')
   for(let i = 0; i < responseJson.quotes.length; i++){
-    $('.quote-list').append(
+    $('.quotes').append(
       `<li> <p> '${responseJson.quotes[i].quoteText}' </p>
       <p> '${responseJson.quotes[i].quoteAuthor}' </p>
       </li>
       `
     )
-
   }
-
 }
-
 function displayPictures(responseJson) {
-  $('.photos').empty();
-  for(let i = 0; i < responseJson.length; i++) {
+  console.log(responseJson, 'hiii')
     $('.photos').append(
-      `<li> <img src ='${responseJson[i].urls.regular}'>
-      <p>${responseJson[i].description}</p>
+      `<li> <img src ='${responseJson.urls.regular}'>
+      <a href = ${responseJson.urls.regular}> Unsplash Link </a>
       </li>
       `
     )
-  }
 }
-
-
 function render() {
-  $(generateWelcomePage)
   $(watchForm);
 }
-
 $(render);
