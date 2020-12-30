edfab4b050b47ec04c2ad44858f2058cf376a8fc
acheckler <allisonheckler@gmail.com>
2020-12-30 17:26:56 -0600

    added favicon & other changes after feedback

--- script.js
+++ script.js
@@ -1,116 +1,136 @@
-'use strict'
-const unsplashID = "RB0c5XN-C4SiX0b5hQwEgq_-FSZ1zi00xCtLXON7UY0"
-const unsplashURL = "https://api.unsplash.com/search/photos"
-const qgURL = "https://quote-garden.herokuapp.com/api/v3/quotes"
+"use strict";
+const unsplashID = "RB0c5XN-C4SiX0b5hQwEgq_-FSZ1zi00xCtLXON7UY0";
+const unsplashURL = "https://api.unsplash.com/search/photos";
+const qgURL = "https://quote-garden.herokuapp.com/api/v3/quotes";
 
+//EVENT LISTENERS//
 function watchStartBtn() {
-  $('#start-button').on("click", () => {
-    $('#js-form').removeClass('hidden');
-    $('.search-instructions').removeClass('hidden');
-    $('.hide-landing-page').addClass('hidden');
-  })
-}
-
-
-function watchForm() {
-  $('#js-form').submit(event => {
-    event.preventDefault();
-    var picSearchTerm = $('#photo-search-term').val();
-    var quoteSearchType = $('#quote-search-type').val();
-    var quoteSearchTerm = $('#quote-search-term').val();
-    var picColor = $('#photo-search-color').val();
-    getPictures(picSearchTerm, picColor);
-    getQuotes(quoteSearchTerm, quoteSearchType);
-    $('.photos').empty();
-    $('.quotes').empty();
-
+  $("#start-button").on("click", () => {
+    $("#js-form").removeClass("hidden");
+    $(".search-instructions").removeClass("hidden");
+    $(".hide-landing-page").addClass("hidden");
   });
 }
 
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+function watchForm() {
+  $("#js-form").submit((event) => {
+    event.preventDefault();
+    var picSearchTerm = $("#photo-search-term").val();
+    var quoteSearchType = $("#quote-search-type").val();
+    var quoteSearchTerm = $("#quote-search-term").val();
+    var picColor = $("#photo-search-color").val();
+    getPictures(picSearchTerm, picColor);
+    getQuotes(quoteSearchTerm, quoteSearchType);
+    $(".photos").empty();
+    $(".quotes").empty();
+    $("#error-message").empty();
+  });
 }
+
+//Formatting user input for get request//
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
+}
+
+//FETCH FUNCTIONS//
+
+//get photos from unsplash//
 function getPictures(picSearchTerm, picColor) {
   const params = {
     client_id: unsplashID,
     query: picSearchTerm,
     per_page: 50,
-    color: picColor
-  }
+    color: picColor,
+  };
   const queryString = formatQueryParams(params);
-  const url = unsplashURL + '?' + queryString;
-  const getRandom = max => {
-    return Math.ceil(Math.random() * max)
-  }
+  const url = unsplashURL + "?" + queryString;
+  const getRandom = (max) => {
+    return Math.ceil(Math.random() * max);
+  };
   fetch(url)
-    .then(response => {
+    .then((response) => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => {
-      let total = responseJson.results.length
-      let random = getRandom(total)
+    .then((responseJson) => {
+      let total = responseJson.results.length;
+      let random = getRandom(total);
       displayPictures(responseJson.results[random]);
     })
-    .catch(err => {
-      $('#error-message').text(`Could not find a photo with given keyword(s). Please try using another search term.`);
+    .catch((err) => {
+      $("#error-message").text(
+        `Could not find a photo with given keyword(s). Please try using another search term.`
+      );
     });
 }
+
+//get quotes from quote garden//
 function getQuotes(quoteSearchTerm, quoteSearchType) {
   const params = {
     page: 1,
-    limit: 3
-  }
+    limit: 3,
+  };
   if (quoteSearchType === "quotes") {
-    params.query = quoteSearchTerm
-  }
-  else {
-    params.genre = quoteSearchTerm
+    params.query = quoteSearchTerm;
+  } else {
+    params.genre = quoteSearchTerm;
   }
   const queryString = formatQueryParams(params);
-  const url = qgURL + '?' + queryString;
+  const url = qgURL + "?" + queryString;
   fetch(url)
-    .then(response => {
+    .then((response) => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => {
+    .then((responseJson) => {
       displayQuotes(responseJson);
     })
-    .catch(err => {
-
-      $('#error-message').text(`Could not find a quote with given keyword(s). Please try using another search term.`);
+    .catch((err) => {
+      $("#error-message").text(
+        `Could not find a quote with given keyword(s). Please try using another search term.`
+      );
     });
 }
+
+//DATA DISPLAY FUNCTIONS//
+
+//Display text returned from QG//
 function displayQuotes(responseJson) {
   if (responseJson.data.length === 0) {
-    $('#error-message').text(`Could not find a quote with given keyword(s). Please try using another search term.`);
+    $("#error-message").text(
+      `Could not find a quote with given keyword(s). Please try using another search term.`
+    );
   }
   for (let i = 0; i < responseJson.data.length; i++) {
-    $('.quotes').append(
+    $(".quotes").append(
       `<li> <p> '${responseJson.data[i].quoteText}' </p>
       <p> '${responseJson.data[i].quoteAuthor}' </p>
       </li>
       `
-    )
+    );
   }
 }
+
+//display pictures returned from unsplash//
 function displayPictures(responseJson) {
-  $('.photos').append(
+  $(".photos").append(
     `<div class="results-image"
     <li> <img src ='${responseJson.urls.regular}' id="fetch-image" alt = "Image returned from API">
       <a href = ${responseJson.urls.regular}> Unsplash Link </a>
       </li>
       </div>
       `
-  )
+  );
 }
+
+//All purpose render function//
 function render() {
   $(watchForm);
   $(watchStartBtn);
